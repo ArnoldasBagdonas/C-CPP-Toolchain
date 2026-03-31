dcf8156ecb642e1833c7b76302297b7030b7583e
Arnoldas Bagdonas <arnoldas.bagdonas@gmail.com>
2026-03-31 22:01:02 +0300

    feat: switched to prebuilt third_party with CMake package support

---
+++ host-app/third_party/src/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /workspace/host-app/third_party/src/googletest-src
+# Build directory: /workspace/host-app/third_party/src/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ host-app/third_party/src/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspace/host-app/third_party/src/googletest-src/googletest
+# Build directory: /workspace/host-app/third_party/src/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
